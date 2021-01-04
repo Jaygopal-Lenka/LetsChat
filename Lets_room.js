127e21ca641ff4f6352e7d9a579d4d409f9de278
Jaygopal-Lenka <73235238+Jaygopal-Lenka@users.noreply.github.com>
2021-01-04 17:57:02 +0530

    Add files via upload

--- Lets_room.js
+++ Lets_room.js
@@ -14,11 +14,25 @@ var firebaseConfig = {
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
    //Start code
-
+   row= "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML +=row;
    //End code
    });});}
 getData();
 
 function logout() {
    window.location="index.html";
+}
+
+
+function addRoom() {
+
+   room_name= document.getElementById("room_name_input").value;
+
+   firebase.database().ref("/").child(room_name).update({
+         purpose: "adding room name"});
+
+   localStorage.setItem("room_name",room_name);
+
+   window.location= "LetsChat_page.html"
 }
